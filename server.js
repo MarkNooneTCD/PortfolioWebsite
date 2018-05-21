b2441c08eca0ff7d83df652ef6ae2e67a948783e
MarkNooneTCD <manoone@tcd.ie>
2018-05-21 23:58:47 +0200

    Added several design pages, bitmoji favicon, and appended server for design page functionality

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const express = require('express'),
 const app = express();
 
 app.get('/', (req, res) => res.sendfile("./public/index.html"));
-app.get('/about.html', (req, res) => res.sendfile("./public/about.html"));
+app.get('/about', (req, res) => res.sendfile("./public/about.html"));
+app.get('/acuacica', (req, res) => res.sendfile("./public/acuacica.html"));
+app.get('/cjsgaff', (req, res) => res.sendfile("./public/cjsgaff.html"));
+app.get('/syze', (req, res) => res.sendfile("./public/syze.html"));
+app.get('/stoury', (req, res) => res.sendfile("./public/stoury.html"));
+app.get('/renderhood', (req, res) => res.sendfile("./public/renderhood.html"));
 
 app.use(express.static(__dirname + '/public'));
 app.listen(3000, () => {
